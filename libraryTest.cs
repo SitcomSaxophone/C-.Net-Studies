62f36109743afb70daf94751a8c4426839a06f93
Samuel Solberg <pauliesays65@gmail.com>
2019-08-09 14:36:35 -0500

    finished library test file

--- libraryTest.cs
+++ libraryTest.cs
@@ -10,7 +10,7 @@ namespace StringLibraryTest
         [TestMethod]
         public void TestStartsWithUpper()
         {
-            //Test what we expect to return true
+            // Test what we expect to return true
             string[] words = {"Alphabet", "Zebra", "ABC", "Αθήνα", "Москва"};
             foreach(var word in words) 
             {
@@ -18,6 +18,34 @@ namespace StringLibraryTest
                 Assert.IsTrue(result,
                         String.Format("Expected for '{0}': true; Actual: {1}",
                                       word, result));
+            }
+        }
+
+        [TestMethod]
+        public void TestDoesNotStartWithUpper() 
+        {
+            // Test what we expect to return false
+            string[] words = {"pelican", "desERT", "spoon", "1234", "."};
+            foreach(var word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.IsFalse(result,
+                        String.Format("Expected for '{0}: false; Actual: {1}",
+                                        word, result));
+            }
+        }
+
+        [TestMethod]
+        public void DirectCallWithNullOrEmpty()
+        {
+            // Test what we expect to return false
+            string[] words = {String.Empty, null};
+            foreach(var word in words)
+            {
+                bool result = StringLibrary.StartsWithUpper(word);
+                Assert.IsFalse(result,
+                        String.Format("Expected for '{0}': false; Actual: {1}",
+                                        word == null ? "<null>" : word, result));
             }
         }
     }
